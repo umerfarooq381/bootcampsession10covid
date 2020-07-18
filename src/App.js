624279ba86b8e7e72c67d48f10214162e34d5172
Umer Farooq <uumerfarooq01@gmail.com>
2020-07-19 01:52:53 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NavBar from './Components/NavBar'
+import InfoPanel from './Components/InfoPanel'
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <InfoPanel />
+    </div>
+  );
+}
+
+export default App;
